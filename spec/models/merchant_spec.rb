65a4638c16ccd5940ceed2c04fcb904611badf05
Will Wang <willy.j.wang@gmail.com>
2022-12-09 11:00:39 -0600

    Add some class method tests to model tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices) }
   end
   describe 'class methods' do
+    it '#find_name' do
+      merchant1 = create(:merchant, name: "Will")
+      merchant2 = create(:merchant, name: "Akhil")
+      merchant3 = create(:merchant, name: "Dustin")
+      merchant4 = create(:merchant, name: "Z WILL")
 
+      expect(Merchant.find_name("Will")).to eq(merchant1)
+    end
   end
 end
