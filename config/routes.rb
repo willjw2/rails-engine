be814467591eb7b1db3b8c7df7ce249eec0fab73
Will Wang <willy.j.wang@gmail.com>
2022-12-09 02:18:17 -0600

    Add endpoint to find merchant associated with an item

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
-      resources :items, only: [:index, :show, :create, :update, :destroy]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index]
+      end
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        resource :merchant, only: [:show]
+      end
     end
   end
 end
